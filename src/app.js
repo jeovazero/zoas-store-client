bf8d62c5ba0ed4c04d79554948fac6ba28c08b68
jeovazero <jeovapereira2000@live.com>
2019-05-08 00:28:22 -0300

    chore: add base config

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => (
+  <div>
+    <h3>
+      Hello friend!
+    </h3>
+    <p>
+      Zoas Store - only for zoas
+    </p>
+  </div>
+)
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'zoas_bundle.[hash].js'
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+    port: 8081
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: '/node_modules/',
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlPlugin({
+      template: './src/index.html'
+    }),
+  ]
+}
