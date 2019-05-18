cb6974057a31c3307ff00752c1dfbfcc34ca60f7
jeovazero <jeovapereira2000@live.com>
2019-05-18 18:09:02 -0300

    chore: add a initial theme

--- src/app.js
+++ src/app.js
@@ -1,14 +1,26 @@
 import React from 'react'
+import { MuiThemeProvider } from '@material-ui/core/styles'
+import theme from './theme'
+import Button from '@material-ui/core/Button'
 
 const App = () => (
-  <div>
-    <h3>
-      Hello friend!
-    </h3>
-    <p>
-      Zoas Store - only for zoas
-    </p>
-  </div>
+  <MuiThemeProvider theme={theme}>
+    <div>
+      <h3>
+        Hello friend!
+      </h3>
+      <p>
+        Zoas Store - only for zoas
+      </p>
+      <Button
+        variant='contained'
+        color='primary'>
+        A dark button
+      </Button>
+      <Button variant='contained' color='secondary'> A arcane button </Button>
+      <Button variant='outlined' color='primary'> A arcane button </Button>
+    </div>
+  </MuiThemeProvider>
 )
 
 export default App
---
+++ src/theme.js
@@ -0,0 +1,17 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const palette = {
+  primary: { main: '#202020' },
+  secondary: { main: '#00E5FF' }
+};
+
+const themeName = 'Mine Shaft Cyan / Aqua Chuckwalla';
+
+const typography = {
+  fontFamily: 'Koho, Roboto, sans-serif',
+  button: {
+    fontWeight: 'bold'
+  }
+}
+
+export default createMuiTheme({ palette, themeName, typography });
