e348b8bbcf18210fe6265dbe75f0bf2086ee4087
jeovazero <jeovapereira2000@live.com>
2019-05-20 17:16:59 -0300

    chore(storybook): add knobs and info addons

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,12 +1,17 @@
 import { configure, addDecorator } from '@storybook/react'
 import { muiTheme } from 'storybook-addon-material-ui'
+import { withInfo } from '@storybook/addon-info'
+import { withKnobs } from '@storybook/addon-knobs'
 import theme from '../src/theme'
+import './style.css'
+
+addDecorator(withInfo)
+addDecorator(withKnobs)
+addDecorator(muiTheme([theme]))
 
 const req = require.context('../src/components', true, /.stories.js$/)
 function loadStories () {
   req.keys().forEach(filename => req(filename))
 }
-
-addDecorator(muiTheme([theme]))
 
 configure(loadStories, module)
--- src/components/ButtonPrimary/ButtonPrimary.stories.js
+++ src/components/ButtonPrimary/ButtonPrimary.stories.js
@@ -1,8 +1,10 @@
 // @flow
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { text } from '@storybook/addon-knobs'
 import ButtonPrimary from './ButtonPrimary'
 
-storiesOf('Components', module).add('Button', () => (
-  <ButtonPrimary> Primary </ButtonPrimary>
-))
+storiesOf('Components', module).add('Button', () => {
+  const label = text('Label', 'Primary')
+  return <ButtonPrimary> {label} </ButtonPrimary>
+})
