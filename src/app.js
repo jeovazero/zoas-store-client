91348ac961470502708ba04b59e3fd87fcde0d6b
jeovazero <jeovapereira2000@live.com>
2019-05-26 19:07:42 -0300

    refactor(app): change theme provider

--- src/app.js
+++ src/app.js
@@ -2,16 +2,16 @@ import React from 'react'
 import theme from './theme'
 import { Home } from './pages'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { MuiThemeProvider } from '@material-ui/core/styles'
+import { ThemeProvider } from '@material-ui/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 
 const App = () => (
-  <MuiThemeProvider theme={theme}>
+  <ThemeProvider theme={theme}>
     <CssBaseline />
     <Router>
       <Route exact path='/' component={Home} />
     </Router>
-  </MuiThemeProvider>
+  </ThemeProvider>
 )
 
 export default App
