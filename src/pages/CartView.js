75f822d5f381863873268eff8189e6c4e81c7c0d
jeovazero <jeovapereira2000@live.com>
2019-05-28 15:46:30 -0300

    feat: add button for checkout

--- src/pages/CartView.js
+++ src/pages/CartView.js
@@ -3,27 +3,41 @@ import React from 'react'
 import relay from '../createRelay'
 import { ProductCartList } from './containers'
 import CartRootRender from './CartRootRender'
-import { CircularProgress } from '@material-ui/core'
+import { CircularProgress, Typography, Button } from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles'
 import { graphql, QueryRenderer } from 'react-relay'
 
 const useStyles = makeStyles({
   root: {
     display: 'flex',
-    justifyContent: 'center'
+    alignItems: 'center',
+    flexDirection: 'column'
   },
   loader: {
     padding: '2rem'
+  },
+  total: {
+    marginTop: '1rem',
+    display: 'flex',
+    justifyContent: 'space-between',
+    flexGrow: 1,
+    marginBottom: '2rem'
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+    maxWidth: '412px',
+    width: '100%'
   }
 })
 
 const CartView = () => {
   const classes = useStyles()
-
+  const total = c => c.reduce((acc, cur) => acc + cur.quantity * cur.price, 0)
   return (
     <div>
       <CartRootRender>
-        {({ refetchCart }) => (
+        {({ refetchCart, cart }) => (
           <div className={classes.root}>
             <QueryRenderer
               environment={relay}
@@ -51,6 +65,15 @@ const CartView = () => {
                 )
               }}
             />
+            <div className={classes.details}>
+              <div className={classes.total}>
+                <Typography variant='h3'>Total</Typography>
+                <Typography variant='h3'>{`R$ ${total(cart)}`}</Typography>
+              </div>
+              <Button size='large' variant='contained' color='primary'>
+                Checkout
+              </Button>
+            </div>
           </div>
         )}
       </CartRootRender>
