085263d8ed55c93be7bbcb0b7647b4442536e4d7
jeovazero <jeovapereira2000@live.com>
2019-06-01 21:48:33 -0300

    feat(app-bar): change props and tests

--- src/components/AppBarZoas/AppBarZoas.js
+++ src/components/AppBarZoas/AppBarZoas.js
@@ -7,11 +7,12 @@ import { CartBadge } from '..'
 import { makeStyles } from '@material-ui/core/styles'
 
 type Props = {
-  onClick?: () => mixed,
+  onClickLogo?: () => mixed,
+  onClickCart?: () => mixed,
   /** Total price of products in the cart */
-  totalPrice: string,
+  totalPrice: number,
   /** Quantity of products in the cart */
-  totalProducts: string,
+  totalProducts: number,
   /** className */
   className?: string
 }
@@ -20,24 +21,42 @@ const useStyles = makeStyles(theme => ({
   imgContainer: {
     flexGrow: 1,
     display: 'flex',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    cursor: 'pointer'
+  },
+  toolbar: {
+    [theme.breakpoints.up('md')]: {
+      width: '940px',
+      margin: 'auto',
+      boxSizing: 'border-box'
+    }
   }
 }))
 
 const AppBarZoas = (props: Props) => {
-  const { totalPrice, totalProducts, className, onClick } = props
+  const {
+    totalPrice,
+    totalProducts,
+    className,
+    onClickLogo,
+    onClickCart
+  } = props
   const classes = useStyles()
 
   return (
-    <AppBar position='static' className={className}>
-      <Toolbar>
-        <div className={classes.imgContainer}>
-          <img src={ZoasLogo} />
+    <AppBar
+      position='static'
+      className={[className, classes.appbar].join(' ')}
+      aria-label='barra do app'
+    >
+      <Toolbar className={classes.toolbar}>
+        <div className={classes.imgContainer} onClick={onClickLogo}>
+          <img src={ZoasLogo} alt='zoas logo' aria-label='zoas logo' />
         </div>
         <CartBadge
           quantity={totalProducts}
           label={totalPrice}
-          onClick={onClick}
+          onClick={onClickCart}
         />
       </Toolbar>
     </AppBar>
--- src/components/AppBarZoas/AppBarZoas.stories.js
+++ src/components/AppBarZoas/AppBarZoas.stories.js
@@ -2,10 +2,19 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { text } from '@storybook/addon-knobs'
+import { action } from '@storybook/addon-actions'
 import AppBarZoas from './AppBarZoas'
 
 storiesOf('Components', module).add('AppBarZoas', () => {
   const quantity = text('Quantity', '10')
   const price = text('Price', 'R$ 10,00')
-  return <AppBarZoas totalProducts={quantity} totalPrice={price} />
+
+  return (
+    <AppBarZoas
+      totalProducts={quantity}
+      totalPrice={price}
+      onClickCart={action('AppBarZoas :: onClickCart')}
+      onClickLogo={action('AppBarZoas :: onClickLogo')}
+    />
+  )
 })
