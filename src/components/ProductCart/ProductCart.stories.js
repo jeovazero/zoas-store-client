1adf03b296bc8a371bb8b7021eef9d53c7f60819
jeovazero <jeovapereira2000@live.com>
2019-06-03 16:13:57 -0300

    refactor(product-cart): update component, stories, tests

--- src/components/ProductCart/ProductCart.stories.js
+++ src/components/ProductCart/ProductCart.stories.js
@@ -1,7 +1,7 @@
 // @flow
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { text } from '@storybook/addon-knobs'
+import { text, number } from '@storybook/addon-knobs'
 import { action } from '@storybook/addon-actions'
 import ProductCart from './ProductCart'
 
@@ -11,15 +11,13 @@ storiesOf('Components', module).add('ProductCart', () => {
     'https://res.cloudinary.com/sohdezoas/image/upload/v1556567196/zoas/Group_3_z9s6ep.jpg'
   )
   const title = text('title', 'Shirt model 2')
-  const subtitle = text('subtitle', 'A shirt of zoas mark')
-  const price = text('price', 2.0)
-  const quantity = text('quantity', 2)
+  const price = number('price', 2.0)
+  const quantity = number('quantity', 2)
 
   return (
     <ProductCart
       title={title}
       image={image}
-      subtitle={subtitle}
       price={price}
       quantity={quantity}
       onChangeQuantity={action('ProductCart :: OnQuantity')}
