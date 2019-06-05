beaa3ed57ddef8b1a1c524522c31ff784e597875
jeovazero <jeovapereira2000@live.com>
2019-06-05 02:55:37 -0300

    feat: add snackbar-error

--- src/components/common.js
+++ src/components/common.js
@@ -1,7 +1,16 @@
+// @flow
 import React from 'react'
 import { styled } from '@material-ui/styles'
-import { CircularProgress, Typography } from '@material-ui/core'
+import {
+  IconButton,
+  CircularProgress,
+  Typography,
+  Snackbar,
+  SnackbarContent
+} from '@material-ui/core'
+import { Error as ErrorIcon, Close as CloseIcon } from '@material-ui/icons'
 
+// CenterWrapper
 export const CenterWrapper = styled('div')(({ theme }) => ({
   display: 'flex',
   alignItems: 'center',
@@ -10,11 +19,60 @@ export const CenterWrapper = styled('div')(({ theme }) => ({
   maxWidth: theme.breakpoints.values['md']
 }))
 
+// Loader
 export const Loader = styled(CircularProgress)({
   padding: '2rem'
 })
 
+// Title
 export const Title = styled(props => <Typography variant='h4' {...props} />)({
   padding: '2rem 1rem',
   width: '100%'
 })
+
+// Snackbar
+const SnackbarContentError = styled(SnackbarContent)(({ theme }) => ({
+  backgroundColor: theme.palette.error.dark,
+  flexWrap: 'nowrap'
+}))
+
+const SnackMessage = styled('span')({
+  display: 'flex',
+  alignItems: 'center',
+  '& svg': {
+    marginRight: '4px'
+  }
+})
+
+type SnackbarProps = {
+  onClose: () => mixed,
+  message?: string
+}
+
+export const SnackbarError = (props: SnackbarProps) => {
+  const {
+    onClose,
+    message = 'Um erro inesperado ocorreu! Tente novamente mais tarde',
+    ...spread
+  } = props
+  return (
+    <Snackbar
+      anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      {...spread}
+    >
+      <SnackbarContentError
+        message={
+          <SnackMessage>
+            <ErrorIcon />
+            {message}
+          </SnackMessage>
+        }
+        action={[
+          <IconButton key='close' color='inherit' onClick={onClose}>
+            <CloseIcon />
+          </IconButton>
+        ]}
+      />
+    </Snackbar>
+  )
+}
--- src/pages/HomeView.js
+++ src/pages/HomeView.js
@@ -4,9 +4,15 @@ import relayEnv from '../relay/createRelay'
 import AppBarRender from '../relay/common/AppBarRender'
 import { ProductList } from '../relay/containers'
 import { graphql, QueryRenderer } from 'react-relay'
-import { CenterWrapper, Title, Loader } from '../components/common'
+import {
+  CenterWrapper,
+  Title,
+  Loader,
+  SnackbarError
+} from '../components/common'
 
 const Home = () => {
+  const [isOpenError, setIsOpenError] = React.useState(false)
   return (
     <AppBarRender>
       {() => (
@@ -24,13 +30,18 @@ const Home = () => {
             variables={{}}
             render={({ error, props }) => {
               if (error) {
-                return <p>{error.message}</p>
+                setIsOpenError(true)
+                return <Loader />
               }
               if (!props) {
                 return <Loader />
               }
               return <ProductList data={props.products} />
             }}
+          />
+          <SnackbarError
+            open={isOpenError}
+            onClose={() => setIsOpenError(false)}
           />
         </CenterWrapper>
       )}
