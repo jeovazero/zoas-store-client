d8cc2bfbc0587cc9b368b9974835b53b99ef9d99
jeovazero <jeovapereira2000@live.com>
2019-06-15 00:54:36 -0300

    chore: update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,40 +2,63 @@ const HtmlPlugin = require('html-webpack-plugin')
 const path = require('path')
 const webpack = require('webpack')
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'zoas_bundle.[hash].js'
-  },
-  devServer: {
-    contentBase: './dist',
-    hot: true,
-    port: 8081
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: '/node_modules/',
-        use: {
-          loader: 'babel-loader'
+module.exports = (_, argv) => {
+  const isProduction = argv.mode === 'production'
+  return {
+    entry: {
+      zoasapp: './src/index.js'
+    },
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: isProduction ? '[name].[contenthash].js' : '[name].js'
+    },
+    devServer: {
+      contentBase: './dist',
+      hot: true,
+      port: 8081
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: '/node_modules/',
+          use: {
+            loader: 'babel-loader'
+          }
+        },
+        {
+          test: /(\.svg|\.png|\.jpg)$/,
+          use: {
+            loader: 'file-loader'
+          }
         }
-      },
-      {
-        test: /(\.svg|\.png|\.jpg)$/,
-        use: {
-          loader: 'file-loader'
+      ]
+    },
+    optimization: {
+      minimize: isProduction,
+      runtimeChunk: 'single',
+      splitChunks: {
+        name: false,
+        cacheGroups: {
+          common: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all'
+          }
         }
       }
+    },
+    plugins: [
+      new HtmlPlugin({
+        template: './src/index.html',
+        inject: true,
+        hash: true,
+        cache: true
+      }),
+      new webpack.HashedModuleIdsPlugin(),
+      new webpack.EnvironmentPlugin({
+        API_URL: process.env.API_URL || 'http://localhost:3000'
+      })
     ]
-  },
-  plugins: [
-    new HtmlPlugin({
-      template: './src/index.html'
-    }),
-    new webpack.EnvironmentPlugin({
-      API_URL: process.env.API_URL || 'http://localhost:3000'
-    })
-  ]
+  }
 }
